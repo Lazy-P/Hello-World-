f7201ff3701cab199da9eef18a475a9c0f0364ea
Lazy-P <37202865+Lazy-P@users.noreply.github.com>
2018-04-10 12:11:33 -0400

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,9 @@
+public class Concat {
+    public static void main (String [] args) {
+        String part_1 = "Hello,";
+        String part_3 = " World";
+        String concat = part_1.concat(part_3);
+        System.out.println(concat);
+
+    }
+}
