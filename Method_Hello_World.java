21dcd6b013b7708190bae7f0602091b67801d295
Lazy-P <37202865+Lazy-P@users.noreply.github.com>
2018-04-10 11:51:21 -0400

    Add files via upload

---
+++ Method_Hello_World.java
@@ -0,0 +1,9 @@
+public class Method_Hello_World {
+    public static void main (String [] args) {
+        intro("Hello, World");
+    }
+
+    public static void intro (String s) {
+        System.out.println(s);
+    }
+}
